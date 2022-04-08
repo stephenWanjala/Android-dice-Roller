07c1c436d96fdd6ec4136d8fa8369c31d55742d1
wanb254 <stephenwanjala145@gmail.com>
2022-04-08 17:55:36 +0300

    roll

--- app/src/main/java/com/wtech/diceroller/MainActivity.kt
+++ app/src/main/java/com/wtech/diceroller/MainActivity.kt
@@ -2,10 +2,35 @@ package com.wtech.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button=findViewById(R.id.btn_roll)
+
+    }
+
+    fun rollDice(view: View) {
+        val dicedImage:ImageView=findViewById(R.id.diceImage)
+        val imageResource=when(Random.nextInt(6)+1){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            6->R.drawable.dice_6
+
+            else -> R.drawable.empty_dice
+        }
+        dicedImage.setImageResource(imageResource)
+
     }
 }
